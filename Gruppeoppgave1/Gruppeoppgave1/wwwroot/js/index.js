f235703dbf32d072ebb929004765f7038f60ba9a
WilliamNerem <william-kn@hotmail.com>
2021-09-14 11:47:43 +0200

    Lagt til validering på formen på index og lagt til tom betalingsside

--- Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/index.js
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/index.js
@@ -20,11 +20,18 @@ function formaterStrekninger(strekninger) {
 }
 
 function hentPris() {
+    const prisVoksne = $('#antallVoksne').val();
+    const antallBarn = $('#antallBarn').val();
+    let prisBarn = 0;
+    if (antallBarn != 0) {
+        prisBarn = antallBarn / 2;
+    }
+    const pris = parseInt(prisVoksne) + parseFloat(prisBarn);
     const value = $('#strekningene').val();
     const url = "Strekning/HentEn?id=" + value;
 
     $.get(url, function (strekning) {
-        $("#pris").html(strekning.pris);
+        $("#pris").html(strekning.pris * pris);
     });
 }
 
@@ -32,4 +39,46 @@ function datepicker() {
     $("#startdate").datepicker({ dateFormat: "dd-mm-yy" }).val();
 
     $("#enddate").datepicker({ dateFormat: "dd-mm-yy" }).val();
-};
+}
+
+function validerForm() {
+    const valgtStrekning = $('#strekningene').val();
+    const valgtStartDato = $("#startdate").datepicker({ dateFormat: "dd-mm-yy" }).val();
+    const valgtSluttDato = $("#enddate").datepicker({ dateFormat: "dd-mm-yy" }).val();
+    const valgtAntallVoksne = $('#antallVoksne').val();
+    let error = false;
+
+    console.log(valgtStartDato);
+
+    if (valgtStrekning == null) {
+        $('#errorStrekning').css({ visibility: "visible" });
+        error = true;
+    } else {
+        $('#errorStrekning').css({ visibility: "hidden" });
+    }
+
+    if (valgtStartDato == "") {
+        $('#errorStartDato').css({ visibility: "visible" });
+        error = true;
+    } else {
+        $('#errorStartDato').css({ visibility: "hidden" });
+    }
+
+    if (valgtSluttDato == "") {
+        $('#errorSluttDato').css({ visibility: "visible" });
+        error = true;
+    } else {
+        $('#errorSluttDato').css({ visibility: "hidden" });
+    }
+
+    if (valgtAntallVoksne == 0) {
+        $('#errorAntallVoksne').css({ visibility: "visible" });
+        error = true;
+    } else {
+        $('#errorAntallVoksne').css({ visibility: "hidden" });
+    }
+
+    if (error) {
+        return false;
+    }
+}
