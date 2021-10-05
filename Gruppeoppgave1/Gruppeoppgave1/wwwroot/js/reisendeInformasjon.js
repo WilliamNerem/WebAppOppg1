138cfdab173bb2460a78dc71f85af2027c55e9f4
Maren <maren.lund@wemail.no>
2021-10-05 14:21:46 +0200

    added autocomplete

--- Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/reisendeInformasjon.js
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/reisendeInformasjon.js
@@ -45,18 +45,18 @@ function renderForm() {
             "<h5>Voksen " + (i + 1) + ":</h5>" +
             "<div class='col-6 row' style='margin-right: 10px;'>" +
             "<label class='form-label'>Fornavn:</label>" +
-            "<input id='fornavn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn'/>" +
+            "<input id='fornavn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn' autocomplete='given-name'/>" +
             "<div id='errorFornavn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</div>" +
             "<label class='form-label'>E-post:</label>" +
-            "<input id='email" + (i + 1) + "' class='form-control' type='text' placeholder='E-post' />" +
+            "<input id='email" + (i + 1) + "' class='form-control' type='text' placeholder='E-post' autocomplete='email' />" +
             "<div id='errorEmail" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn e-post!</div>" +
             "</div>" +
             "<div class='col-6 row'>" +
             "<label class='form-label'>Etternavn:</label>" +
-            "<input id='etternavn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' />" +
+            "<input id='etternavn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' autocomplete='family-name'/>" +
             "<div id='errorEtternavn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</div>" +
             "<label class='form-label'>Fødselsdato:</label>" +
-            "<input id='fodselsdato" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
+            "<input id='fodselsdato" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' autocomplete='bday'/>" +
             "<div id='errorFodselsdato" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</div>" +
             "</div>" +
             "</div>";
@@ -71,15 +71,15 @@ function renderForm() {
                 "<h5>Barn " + (i + 1) + ":</h5> <br/>" +
                 "<div class='col-6 row' style='margin-right: 10px;'>" +
                 "<label class='form-label'>Fornavn:</label>" +
-                "<input id='fornavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn' />" +
+                "<input id='fornavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn' autocomplete='given-name' />" +
                 "<div id='errorFornavnBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</div>" +
                 "<label class='form-label'>Fødselsdato:</label>" +
-                "<input id='fodselsdatoBarn" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
+                "<input id='fodselsdatoBarn" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' autocomplete='bday'/>" +
                 "<div id='errorFodselsdatoBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</div>" +
                 "</div>" +
                 "<div class='row col-6'>" +
                 "<label class='form-label'>Etternavn:</label>" +
-                "<input id='etternavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' />" +
+                "<input id='etternavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' autocomplete='family-name'/>" +
                 "<div id='errorEtternavnBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</div>" +
                 "<div style='height: 116px'></div>" +
                 "</div>" +
