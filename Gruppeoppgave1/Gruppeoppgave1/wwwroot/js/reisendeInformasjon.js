2acf570b3f9de51cf10a8e53a630ee9241eae274
WilliamNerem <william-kn@hotmail.com>
2021-10-05 14:02:43 +0200

    Fikset styling på reisendeInfo

--- Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/reisendeInformasjon.js
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/reisendeInformasjon.js
@@ -1,5 +1,7 @@
 ﻿let antallVoksne;
 let antallBarn;
+let antallVoksneTeller = 1;
+let antallBarnTeller = 0;
 
 $(function () {
 
@@ -20,46 +22,71 @@ $(function () {
 });
 
 function renderForm() {
-    let ut = "<h3>Skriv inn informasjon om voksne:</h3><br/>";
-
-    ut += "<h5>Voksen " + 1 + ":</h5> <br/>" +
-        "<label>Fornavn:</label>" +
-        "<input id='fornavn' type='text' value='" + fornavn + "' readonly /><br/><br/>" +
-        "<label>Etternavn:</label>" +
-        "<input id='etternavn' type='text' value='" + etternavn + "' readonly /><br/><br/>" +
-        "<label>Email:</label>" +
-        "<input id='email' type='text' value='" + email + "' readonly/><br/><br/>";
+    let ut = "<div class='row reisendeInformasjon-container g-2 form-container' style='margin-right: 14px'>" +
+        "<h5>Voksen " + 1 + ":</h5>" +
+        "<div class='col-6 row' style='margin-right: 10px;'>" +
+        "<label class='form-label'>Fornavn:</label>" +
+        "<input id='fornavn' class='form-control' type='text' value='" + fornavn + "' readonly />" +
+        "<div style='height: 46px'></div>" +
+        "<label class='form-label'>Email:</label>" +
+        "<input id='email' class='form-control' type='text' value='" + email + "' readonly/>" +
+        "<div style='height: 46px'></div>" +
+        "</div>" +
+        "<div class='col-6 row'>" +
+        "<label class='form-label'>Etternavn:</label>" +
+        "<input id='etternavn' class='form-control' type='text' value='" + etternavn + "' readonly />" +
+        "<div style='height: 162px'></div>" +
+        "</div>" +
+        "</div>";
 
     for (let i = 1; i < antallVoksne; i++) {
-        ut += "<h5>Voksen " + (i + 1) + ":</h5> <br/>" +
-            "<label>Fornavn:</label>" +
-            "<input id='fornavn" + (i + 1) + "' type='text' placeholder='Fornavn'/>" +
-            "<p id='errorFornavn" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</p>" +
-            "<label>Etternavn:</label>" +
-            "<input id='etternavn" + (i + 1) + "' type='text' placeholder='Etternavn' />" +
-            "<p id='errorEtternavn" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</p>" +
-            "<label>Email:</label>" +
-            "<input id='email" + (i + 1) + "' type='text' placeholder='Email' />" +
-            "<p id='errorEmail" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn email!</p>" +
-            "<label>Fødselsdato:</label>" +
-            "<input id='fodselsdato" + (i + 1) + "' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
-            "<p id='errorFodselsdato" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</p>";
+        antallVoksneTeller++;
+        ut += "<div class='reisendeInformasjon-container row g-2 form-container' style='margin-right: 14px'>" +
+            "<h5>Voksen " + (i + 1) + ":</h5>" +
+            "<div class='col-6 row' style='margin-right: 10px;'>" +
+            "<label class='form-label'>Fornavn:</label>" +
+            "<input id='fornavn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn'/>" +
+            "<div id='errorFornavn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</div>" +
+            "<label class='form-label'>Email:</label>" +
+            "<input id='email" + (i + 1) + "' class='form-control' type='text' placeholder='Email' />" +
+            "<div id='errorEmail" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn email!</div>" +
+            "</div>" +
+            "<div class='col-6 row'>" +
+            "<label class='form-label'>Etternavn:</label>" +
+            "<input id='etternavn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' />" +
+            "<div id='errorEtternavn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</div>" +
+            "<label class='form-label'>FødselsDato:</label>" +
+            "<input id='fodselsdato" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
+            "<div id='errorFodselsdato" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</div>" +
+            "</div>" +
+            "</div>";
     }
+
+    let utBarn = "";
+
     if (antallBarn > 0) {
-        ut += "<h3>Skriv inn informasjon om barn:</h3><br/>";
+        $('#infoBarn').html("<h3>Skriv inn informasjon om barn:</h3><br/>");
         for (let i = 0; i < antallBarn; i++) {
-            ut += "<h5>Barn " + (i + 1) + ":</h5> <br/>" +
-                "<label>Fornavn:</label>" +
-                "<input id='fornavnBarn" + (i + 1) + "' type='text' placeholder='Fornavn' />" +
-                "<p id='errorFornavnBarn" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</p>" +
-                "<label>Etternavn:</label>" +
-                "<input id='etternavnBarn" + (i + 1) + "' type='text' placeholder='Etternavn' />" +
-                "<p id='errorEtternavnBarn" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</p>" +
-                "<label>Fødselsdato:</label>" +
-                "<input id='fodselsdatoBarn" + (i + 1) + "' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
-                "<p id='errorFodselsdatoBarn" + (i + 1) + "' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</p>";
+            utBarn += "<div class='row reisendeInformasjon-container g-2 form-container' style='margin-right: 14px'>" +
+                "<h5>Barn " + (i + 1) + ":</h5> <br/>" +
+                "<div class='col-6 row' style='margin-right: 10px;'>" +
+                "<label class='form-label'>Fornavn:</label>" +
+                "<input id='fornavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Fornavn' />" +
+                "<div id='errorFornavnBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fornavn!</div>" +
+                "<label class='form-label'>FødselsDato:</label>" +
+                "<input id='fodselsdatoBarn" + (i + 1) + "' class='form-control' type='text' placeholder='DD-MM-ÅÅÅÅ' />" +
+                "<div id='errorFodselsdatoBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn fødselsdato!</div>" +
+                "</div>" +
+                "<div class='row col-6'>" +
+                "<label class='form-label'>Etternavn:</label>" +
+                "<input id='etternavnBarn" + (i + 1) + "' class='form-control' type='text' placeholder='Etternavn' />" +
+                "<div id='errorEtternavnBarn" + (i + 1) + "' class='form-text' style='visibility: hidden; color: red'>Du må skrive inn etternavn!</div>" +
+                "<div style='height: 116px'></div>" +
+                "</div>" +
+                "</div>";
         }
     }
 
     $('#betalingForm').html(ut);
+    $('#betalingFormBarn').html(utBarn);
 }
