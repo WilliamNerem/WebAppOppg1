dce75c7807692421068a74695e6c8de747f709f5
WilliamNerem <william-kn@hotmail.com>
2021-10-07 17:23:18 +0200

    console log

--- Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/validering.js
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/validering.js
@@ -1,6 +1,6 @@
 let error = false;
 const regexFornavn = /^\b[A-ZÀ-ÿ][a-z ,.'-]+$/;
-const regexEtternavn = /\b([A-ZÀ-ÿ][-a-z ]+[ ]*)+/;
+const regexEtternavn = /^\b[A-ZÀ-ÿ][a-z ,.'-]+$/;
 const regexEmail = /([\w\.\-]+)@([\w\-]+).(\w{2,3})/;
 const regexFodselsdato = /(((0[1-9]|[12][0-9]|3[01])[/](0[13578]|10|12)[/](\b([1][9][0-9][0-9]|[2][0][0-1][0-9]|[2][0][2][0])\b))|(([0][1-9]|[12][0-9]|30)[/](0[469]|11)[/](\b([1][9][0-9][0-9]|[2][0][0-1][0-9]|[2][0][2][0])\b))|((0[1-9]|1[0-9]|2[0-8])[/](02)[/](\b([1][9][0-9][0-9]|[2][0][0-1][0-9]|[2][0][2][0])\b))|((29)(\/)(02)[/]([02468][048]00))|((29)[/](02)[/]([13579][26]00))|((29)[/](02)[/]([0-9][0-9][0][48]))|((29)[/](02)[/]([0-9][0-9][2468][048]))|((29)[/](02)[/]([0-9][0-9][13579][26])))/;
 const regexKortnr = /\b[0-9]{4}[ ][0-9]{4}[ ][0-9]{4}[ ][0-9]{4}\b/;
@@ -91,12 +91,10 @@ function validerBetalingsForm() {
     sjekkRegex(skrevetKortnr, regexKortnr, '#errorKortnummer', "Kortnummer må være gyldig og på formatet: 'XXXX XXXX XXXX XXXX'");
     sjekkRegex(skrevetUtlopsdato, regexUtlopsdato, '#errorUtlopsdato', "Utløpsdato må være gyldig og på formatet: 'MM/ÅÅ'");
     sjekkRegex(skrevetCVC, regexCVC, '#errorCVC', "CVC må være gyldig og på tre sifre");
-    console.log(error);
 
     formValidering(skrevetKortnr, "", '#errorKortnummer', "Du må skrive inn kortnummer!");
     formValidering(skrevetUtlopsdato, "", '#errorUtlopsdato', "Du må skrive inn utløpsdato!");
     formValidering(skrevetCVC, "", '#errorCVC', "Du må skrive inn CVC!");
-    console.log(error);
 
     if (error) {
         error = false;
